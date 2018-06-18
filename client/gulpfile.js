76a1752472f3b4744198f0a83f0c7ec7e20342f9
Francisco Gutierrez <francisco.j.gtz@gmail.com>
2018-06-18 12:29:23 -0700

    Feat: Add gulpfile to watch html

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,15 +1,48 @@
-const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
+const gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  gulpautoprefixer = require('gulp-autoprefixer'),
+  postcss = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer'),
+  cssnano = require('cssnano'),
+  sourcemaps = require('gulp-sourcemaps'),
+  browserSync = require('browser-sync').create();
 
-gulp.task('default', defaultTask);
+const paths = {
+  styles: {
+    src: 'sass/**/*.scss',
+    dest: 'css',
+  },
+};
 
-function defaultTask(done) {
-  // place code for your default task here
-  console.log('testing RR');
-  done();
+function styles() {
+  const plugins = [
+    autoprefixer({ browsers: ['last 2 versions'] }),
+    cssnano(),
+  ];
+  return gulp.src(paths.styles.src)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss(plugins))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(browserSync.reload({
+      stream: true,
+    }));
 }
 
-browserSync.init({
-  server: './',
-});
-browserSync.stream();
+function reload() {
+  browserSync.reload();
+}
+
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: ['./'],
+    },
+  });
+  gulp.watch(paths.styles.src, styles);
+  gulp.watch('*.html', reload);
+}
+
+exports.styles = styles;
+exports.watch = watch;
